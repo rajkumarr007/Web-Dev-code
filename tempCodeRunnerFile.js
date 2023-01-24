8aa84cba39bc374c614fbaf8ac5df37abe1b30ae
rajkumarr007 <123387196+rajkumarr007@users.noreply.github.com>
2023-01-24 19:10:17 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+ // const params = {'message': 'Your form has been submitted successfully'}
+    // res.status(200).render('index.pug', params);
---
+++ tut73.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+//app KI JAGHE PER KUCH OR BHI NAME USE KER SKTE HAI FIR SAB JADHE PER YHI USE KERNA HOGA 
+
+const port = 80;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {'title': 'PubG is the best game', "content": con}
+    res.status(200).render('index.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
